7ca77b11dd4ebec8fb814c065d13d7d0bc5b2836
fares <troll.rar@gmail.com>
2020-11-07 22:46:50 +0100

    moving pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'meme-app';
+  public page = 1;
+
+  public next(): void {
+    this.page++;
+  }
 }
--- src/app/page-one/page-one.component.ts
+++ src/app/page-one/page-one.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-page-one',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-one.component.scss']
 })
 export class PageOneComponent implements OnInit {
+
+  @Output() public next = new EventEmitter<void>();
 
   constructor() { }
 
